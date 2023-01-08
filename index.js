5c9859485729710af85db289f7e5c1bc165eec34
rajeevjaiiswal <rjjaiisw@3c063043a712.ant.amazon.com>
2023-01-08 00:46:50 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//Load express module with `require` directive
+var express = require('express')
+var Request = require("request")
+var app = express()
+
+
+ 
+//Define request response in root URL (/)
+app.get('/*', function (req, res) {
+   
+ 
+  res.send("Order Recieved. Processing started...\n" )
+})
+
+//Launch listening server on port 8080
+app.listen(8090, function () {
+  console.log('Order service listening on port 8090!')
+})
